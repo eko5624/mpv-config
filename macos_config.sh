420f604337508e44159c0369fd4f91dcc8f5b212
eko5624 <562433403@qq.com>
2022-11-06 19:51:41 +0800

    Update macos_config.sh

--- macos_config.sh
+++ macos_config.sh
@@ -20,7 +20,7 @@ mv MPV_lazy/portable_config/script-opts/stats.conf script-opts
 mv MPV_lazy/portable_config/script-opts/thumbfast.conf script-opts
 mv MPV_lazy/portable_config/script-opts/uosc.conf script-opts
 mv MPV_lazy/portable_config/script-opts/ytdl_hook.conf script-opts
-mv MPV_lazy/portable_config/fonts fonts
+mv MPV_lazy/portable_config/fonts ./
 
 rm -rf MPV_lazy
 
