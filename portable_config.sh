6ef872059eea37b30386605a1f4a794013735163
eko5624 <562433403@qq.com>
2022-11-07 18:27:40 +0800

    Update portable_config.sh

--- portable_config.sh
+++ portable_config.sh
@@ -9,21 +9,21 @@ git clone https://github.com/hooke007/MPV_lazy.git --branch main
 mv MPV_lazy/portable_config/scripts/contextmenu_gui scripts
 mv MPV_lazy/portable_config/scripts/input_plus.lua scripts
 mv MPV_lazy/portable_config/scripts/load_plus.lua scripts
-mv MPV_lazy/portable_config/scripts/osc_plus.lua scripts
 mv MPV_lazy/portable_config/scripts/playlistmanager.lua scripts
 mv MPV_lazy/portable_config/scripts/save_global_props.lua scripts
 mv MPV_lazy/portable_config/scripts/thumbfast.lua scripts
+mv MPV_lazy/portable_config/scripts/uosc scripts
 
 mv MPV_lazy/portable_config/script-opts/console.conf script-opts
 mv MPV_lazy/portable_config/script-opts/contextmenu_gui.conf script-opts
 mv MPV_lazy/portable_config/script-opts/contextmenu_gui_engine.conf script-opts
 mv MPV_lazy/portable_config/script-opts/load_plus.conf script-opts
 mv MPV_lazy/portable_config/script-opts/osc.conf script-opts
-mv MPV_lazy/portable_config/script-opts/osc_plus.conf script-opts
 mv MPV_lazy/portable_config/script-opts/playlistmanager.conf script-opts
 mv MPV_lazy/portable_config/script-opts/save_global_props.conf script-opts
 mv MPV_lazy/portable_config/script-opts/stats.conf script-opts
 mv MPV_lazy/portable_config/script-opts/thumbfast.conf script-opts
+mv MPV_lazy/portable_config/script-opts/uosc.conf script-opts
 mv MPV_lazy/portable_config/script-opts/ytdl_hook.conf script-opts
 
 rm -rf MPV_lazy
