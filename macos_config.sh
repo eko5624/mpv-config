e779226c6b7c232df43ffcb2c4f44681c3808c7c
eko5624 <562433403@qq.com>
2022-11-14 16:24:47 +0800

    Update macos_config.sh

--- macos_config.sh
+++ macos_config.sh
@@ -6,25 +6,25 @@ DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 cd $DIR/macos_config 
 git clone https://github.com/hooke007/MPV_lazy.git --branch main
 
-mv MPV_lazy/portable_config/scripts/uosc scripts
-mv MPV_lazy/portable_config/scripts/input_plus.lua scripts
-mv MPV_lazy/portable_config/scripts/playlistmanager.lua scripts
-mv MPV_lazy/portable_config/scripts/save_global_props.lua scripts
-mv MPV_lazy/portable_config/scripts/thumbfast.lua scripts
+mv -f MPV_lazy/portable_config/scripts/uosc scripts
+mv -f MPV_lazy/portable_config/scripts/input_plus.lua scripts
+mv -f MPV_lazy/portable_config/scripts/playlistmanager.lua scripts
+mv -f MPV_lazy/portable_config/scripts/save_global_props.lua scripts
+mv -f MPV_lazy/portable_config/scripts/thumbfast.lua scripts
 
-mv MPV_lazy/portable_config/script-opts/console.conf script-opts
-mv MPV_lazy/portable_config/script-opts/osc.conf script-opts
-mv MPV_lazy/portable_config/script-opts/playlistmanager.conf script-opts
-mv MPV_lazy/portable_config/script-opts/save_global_props.conf script-opts
-mv MPV_lazy/portable_config/script-opts/stats.conf script-opts
-mv MPV_lazy/portable_config/script-opts/thumbfast.conf script-opts
-mv MPV_lazy/portable_config/script-opts/uosc.conf script-opts
-mv MPV_lazy/portable_config/script-opts/ytdl_hook.conf script-opts
-mv MPV_lazy/portable_config/fonts ./
+mv -f MPV_lazy/portable_config/script-opts/console.conf script-opts
+mv -f MPV_lazy/portable_config/script-opts/osc.conf script-opts
+mv -f MPV_lazy/portable_config/script-opts/playlistmanager.conf script-opts
+mv -f MPV_lazy/portable_config/script-opts/save_global_props.conf script-opts
+mv -f MPV_lazy/portable_config/script-opts/stats.conf script-opts
+mv -f MPV_lazy/portable_config/script-opts/thumbfast.conf script-opts
+mv -f MPV_lazy/portable_config/script-opts/uosc.conf script-opts
+mv -f MPV_lazy/portable_config/script-opts/ytdl_hook.conf script-opts
+mv -f MPV_lazy/portable_config/fonts ./
 
 rm -rf MPV_lazy
 
 curl -O https://raw.githubusercontent.com/Eisa01/mpv-scripts/master/scripts/SmartCopyPaste_II.lua 
 curl -O https://raw.githubusercontent.com/Eisa01/mpv-scripts/master/script-opts/SmartCopyPaste_II.conf 
-mv ./SmartCopyPaste_II.lua scripts
-mv ./SmartCopyPaste_II.conf script-opts
+mv -f ./SmartCopyPaste_II.lua scripts
+mv -f ./SmartCopyPaste_II.conf script-opts
