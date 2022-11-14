2bc1c25cf2b301a137bf75ef16e9ebded44f8a41
eko5624 <562433403@qq.com>
2022-11-14 16:49:10 +0800

    Update macos_config.sh

--- macos_config.sh
+++ macos_config.sh
@@ -6,21 +6,23 @@ DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 cd $DIR/macos_config 
 git clone https://github.com/hooke007/MPV_lazy.git --branch main
 
-mv -f MPV_lazy/portable_config/scripts/uosc scripts
-mv -f MPV_lazy/portable_config/scripts/input_plus.lua scripts
-mv -f MPV_lazy/portable_config/scripts/playlistmanager.lua scripts
-mv -f MPV_lazy/portable_config/scripts/save_global_props.lua scripts
-mv -f MPV_lazy/portable_config/scripts/thumbfast.lua scripts
+rm -rf scripts/uosc
+cp -f MPV_lazy/portable_config/scripts/input_plus.lua scripts
+cp -f MPV_lazy/portable_config/scripts/playlistmanager.lua scripts
+cp -f MPV_lazy/portable_config/scripts/save_global_props.lua scripts
+cp -f MPV_lazy/portable_config/scripts/thumbfast.lua scripts
+cp -r MPV_lazy/portable_config/scripts/uosc scripts
 
-mv -f MPV_lazy/portable_config/script-opts/console.conf script-opts
-mv -f MPV_lazy/portable_config/script-opts/osc.conf script-opts
-mv -f MPV_lazy/portable_config/script-opts/playlistmanager.conf script-opts
-mv -f MPV_lazy/portable_config/script-opts/save_global_props.conf script-opts
-mv -f MPV_lazy/portable_config/script-opts/stats.conf script-opts
-mv -f MPV_lazy/portable_config/script-opts/thumbfast.conf script-opts
-mv -f MPV_lazy/portable_config/script-opts/uosc.conf script-opts
-mv -f MPV_lazy/portable_config/script-opts/ytdl_hook.conf script-opts
-mv -f MPV_lazy/portable_config/fonts ./
+rm -rf fonts
+cp -f MPV_lazy/portable_config/script-opts/console.conf script-opts
+cp -f MPV_lazy/portable_config/script-opts/osc.conf script-opts
+cp -f MPV_lazy/portable_config/script-opts/playlistmanager.conf script-opts
+cp -f MPV_lazy/portable_config/script-opts/save_global_props.conf script-opts
+cp -f MPV_lazy/portable_config/script-opts/stats.conf script-opts
+cp -f MPV_lazy/portable_config/script-opts/thumbfast.conf script-opts
+cp -f MPV_lazy/portable_config/script-opts/uosc.conf script-opts
+cp -f MPV_lazy/portable_config/script-opts/ytdl_hook.conf script-opts
+cp -f MPV_lazy/portable_config/fonts ./
 
 rm -rf MPV_lazy
 
