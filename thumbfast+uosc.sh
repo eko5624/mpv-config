c5a92e044bd65b2d3c2e835038e40540d631b938
eko5624 <562433403@qq.com>
2022-11-14 17:12:01 +0800

    Update thumbfast+uosc.sh

--- thumbfast+uosc.sh
+++ thumbfast+uosc.sh
@@ -3,20 +3,21 @@ set -x
 
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-mkdir -p $DIR/thumbfast+uosc/scripts
-mkdir -p $DIR/thumbfast+uosc/script-opts
+[ ! -d "$DIR/thumbfast+uosc/scripts" ] || mkdir -p $DIR/thumbfast+uosc/scripts
+[ ! -d "$DIR/thumbfast+uosc/script-opts" ] || mkdir -p $DIR/thumbfast+uosc/script-opts
 
 cd $DIR/thumbfast+uosc
 
 # get thumbfast
 git clone https://github.com/po5/thumbfast.git --branch master
-mv thumbfast/thumbfast.lua scripts  
-mv thumbfast/thumbfast.conf script-opts
+cp -f thumbfast/thumbfast.lua scripts  
+cp -f thumbfast/thumbfast.conf script-opts
 rm -rf thumbfast  
 
 # get uosc
 git clone https://github.com/tomasklaen/uosc.git --branch main
-mv uosc/fonts ./
-mv uosc/scripts/* scripts
-mv uosc/script-opts/uosc.conf script-opts
+rm -rf fonts
+cp -r uosc/fonts ./
+cp -rf uosc/scripts/* scripts
+cp -f uosc/script-opts/uosc.conf script-opts
 rm -rf uosc
