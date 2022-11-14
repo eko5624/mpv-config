466057474f727e8b9edb1eec30185ea56b83ec00
eko5624 <562433403@qq.com>
2022-11-14 16:22:51 +0800

    Update portable_config.sh

--- portable_config.sh
+++ portable_config.sh
@@ -3,36 +3,33 @@ set -x
 
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-cd $DIR/portable_config 
+cd $DIR/portable_config
 git clone https://github.com/hooke007/MPV_lazy.git --branch main
 
-mv MPV_lazy/portable_config/scripts/contextmenu_gui scripts
-mv MPV_lazy/portable_config/scripts/input_plus.lua scripts
-mv MPV_lazy/portable_config/scripts/load_plus.lua scripts
-mv MPV_lazy/portable_config/scripts/playlistmanager.lua scripts
-mv MPV_lazy/portable_config/scripts/save_global_props.lua scripts
-mv MPV_lazy/portable_config/scripts/thumbfast.lua scripts
-mv MPV_lazy/portable_config/scripts/uosc scripts
+mv -f MPV_lazy/portable_config/scripts/input_plus.lua scripts
+mv -f MPV_lazy/portable_config/scripts/load_plus.lua scripts
+mv -f MPV_lazy/portable_config/scripts/playlistmanager.lua scripts
+mv -f MPV_lazy/portable_config/scripts/save_global_props.lua scripts
+mv -f MPV_lazy/portable_config/scripts/thumbfast.lua scripts
+mv -f MPV_lazy/portable_config/scripts/uosc scripts
 
-mv MPV_lazy/portable_config/script-opts/console.conf script-opts
-mv MPV_lazy/portable_config/script-opts/contextmenu_gui.conf script-opts
-mv MPV_lazy/portable_config/script-opts/contextmenu_gui_engine.conf script-opts
-mv MPV_lazy/portable_config/script-opts/load_plus.conf script-opts
-mv MPV_lazy/portable_config/script-opts/osc.conf script-opts
-mv MPV_lazy/portable_config/script-opts/playlistmanager.conf script-opts
-mv MPV_lazy/portable_config/script-opts/save_global_props.conf script-opts
-mv MPV_lazy/portable_config/script-opts/stats.conf script-opts
-mv MPV_lazy/portable_config/script-opts/thumbfast.conf script-opts
-mv MPV_lazy/portable_config/script-opts/uosc.conf script-opts
-mv MPV_lazy/portable_config/script-opts/ytdl_hook.conf script-opts
+mv -f MPV_lazy/portable_config/script-opts/console.conf script-opts
+mv -f MPV_lazy/portable_config/script-opts/load_plus.conf script-opts
+mv -f MPV_lazy/portable_config/script-opts/osc.conf script-opts
+mv -f MPV_lazy/portable_config/script-opts/playlistmanager.conf script-opts
+mv -f MPV_lazy/portable_config/script-opts/save_global_props.conf script-opts
+mv -f MPV_lazy/portable_config/script-opts/stats.conf script-opts
+mv -f MPV_lazy/portable_config/script-opts/thumbfast.conf script-opts
+mv -f MPV_lazy/portable_config/script-opts/uosc.conf script-opts
+mv -f MPV_lazy/portable_config/script-opts/ytdl_hook.conf script-opts
 
-mv MPV_lazy/portable_config/fonts ./
-mv MPV_lazy/portable_config/luts ./
-mv MPV_lazy/portable_config/vs ./
+mv -f MPV_lazy/portable_config/fonts ./
+mv -f MPV_lazy/portable_config/luts ./
+mv -f MPV_lazy/portable_config/vs ./
 
 rm -rf MPV_lazy
 
 curl -O https://raw.githubusercontent.com/Eisa01/mpv-scripts/master/scripts/SmartCopyPaste_II.lua 
 curl -O https://raw.githubusercontent.com/Eisa01/mpv-scripts/master/script-opts/SmartCopyPaste_II.conf 
-mv ./SmartCopyPaste_II.lua scripts
-mv ./SmartCopyPaste_II.conf script-opts
+mv -f ./SmartCopyPaste_II.lua scripts
+mv -f ./SmartCopyPaste_II.conf script-opts
