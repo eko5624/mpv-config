621da6a288a76f5fe628142c249e20076dbc98a6
eko5624 <562433403@qq.com>
2022-11-22 17:48:09 +0800

    Update portable_config.sh

--- portable_config.sh
+++ portable_config.sh
@@ -20,8 +20,9 @@ sed -i 's/text_width_estimation = true/text_width_estimation = false/' scripts/u
 # Change to 4× speed play.
 sed -i 's/"speed", 2/"speed", 4/' scripts/input_plus.lua
 
-# Change stats icon from info to analytics.
+# Change 'stats' icon from 'info' to 'analytics', 'play/pause' icon from 'play_circle/pause_circle' to 'play_arrow/pause'.  
 sed -i 's/command:info_outline/command:analytics/' scripts/uosc/elements/Controls.lua
+sed -i 's/cycle:not_started:pause:no=play_circle\/yes=pause_circle/cycle:play_arrow:pause:no=pause\/yes=play_arrow/' scripts/uosc/elements/Controls.lua
 
 cp -f MPV_lazy/portable_config/script-opts/console.conf script-opts
 cp -f MPV_lazy/portable_config/script-opts/load_plus.conf script-opts
@@ -35,6 +36,13 @@ cp -f MPV_lazy/portable_config/script-opts/ytdl_hook.conf script-opts
 
 # Don't dim screen when triggering menu.
 sed -i 's/curtain_opacity=0.5/curtain_opacity=0/' script-opts/uosc.conf
+
+# Add 'prev/next chapter' button and 'open-file' button.
+sed -i 's/gap,play_pause,gap/open-file,gap,<has_chapter>command:skip_previous:add chapter -1?上一章节,play_pause,<has_chapter>command:skip_next:add chapter 1?下一章节,gap/' script-opts/uosc.conf
+
+#Change thumb size from 300px to 360px
+sed -i 's/max_height=300/max_height=360/' script-opts/thumbfast.conf
+sed -i 's/max_width=300/max_width=360/' script-opts/thumbfast.conf
 
 rm -rf fonts luts vs
 cp -r MPV_lazy/portable_config/fonts ./
