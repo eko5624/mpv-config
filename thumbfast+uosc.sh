c4f7ba8e6309545e1a42f61cb7fe92af8cd12409
eko5624 <562433403@qq.com>
2022-11-23 09:25:11 +0800

    Update thumbfast+uosc.sh

--- thumbfast+uosc.sh
+++ thumbfast+uosc.sh
@@ -12,7 +12,11 @@ cd $DIR/thumbfast+uosc
 git clone https://github.com/po5/thumbfast.git --branch master
 cp -f thumbfast/thumbfast.lua scripts  
 cp -f thumbfast/thumbfast.conf script-opts
-rm -rf thumbfast  
+rm -rf thumbfast
+
+#Change thumb size from 200px to 360px
+sed -i 's/max_height=200/max_height=360/' script-opts/thumbfast.conf
+sed -i 's/max_width=200/max_width=360/' script-opts/thumbfast.conf
 
 # get uosc
 git clone https://github.com/tomasklaen/uosc.git --branch main
@@ -21,3 +25,35 @@ cp -r uosc/fonts ./
 cp -rf uosc/scripts/* scripts
 cp -f uosc/script-opts/uosc.conf script-opts
 rm -rf uosc
+
+# Don't dim screen when triggering menu.
+sed -i 's/curtain_opacity=0.5/curtain_opacity=0/' script-opts/uosc.conf
+
+# Add 'prev/next chapter' button and 'open-file' button.
+sed -i 's/gap,play_pause,gap/open-file,gap,<has_chapter>command:skip_previous:add chapter -1?上一章节,play_pause,<has_chapter>command:skip_next:add chapter 1?下一章节,gap/' script-opts/uosc.conf
+
+sed -i "/subtitles =/i 		['play_pause'] = 'cycle:play_arrow:pause:no=pause\/yes=play_arrow?播放/暂停'," scripts/uosc_shared/elements/Controls.lua
+sed -i "/subtitles =/a		['script-stats'] = 'command:analytics:script-binding stats/display-stats-toggle?统计数据'," scripts/uosc_shared/elements/Controls.lua
+sed -i 's/?Menu/?菜单/' scripts/uosc_shared/elements/Controls.lua
+sed -i 's/?Subtitles/?字幕轨/' scripts/uosc_shared/elements/Controls.lua
+sed -i 's/?Audio/?音频轨/' scripts/uosc_shared/elements/Controls.lua
+sed -i 's/?Audio device/?音频设备/' scripts/uosc_shared/elements/Controls.lua
+sed -i 's/?Video/?视频轨/' scripts/uosc_shared/elements/Controls.lua
+sed -i 's/?Playlist/?播放列表/' scripts/uosc_shared/elements/Controls.lua
+sed -i 's/?Chapters/?章节/' scripts/uosc_shared/elements/Controls.lua
+sed -i 's/?Editions/?版本/' scripts/uosc_shared/elements/Controls.lua
+sed -i 's/?Stream quality/?流品质/' scripts/uosc_shared/elements/Controls.lua
+sed -i 's/?Open file/?加载文件/' scripts/uosc_shared/elements/Controls.lua
+sed -i 's/?Playlist\/Files/?播放列表\/文件浏览器' scripts/uosc_shared/elements/Controls.lua
+sed -i 's/?Previous/?上一个/' scripts/uosc_shared/elements/Controls.lua
+sed -i 's/?Next/?下一个/' scripts/uosc_shared/elements/Controls.lua
+sed -i 's/?First/?首位/' scripts/uosc_shared/elements/Controls.lua
+sed -i 's/?Last/?末位/' scripts/uosc_shared/elements/Controls.lua
+sed -i 's/?Loop playlist/?列表循环/' scripts/uosc_shared/elements/Controls.lua
+sed -i 's/?Loop file/?单曲循环/' scripts/uosc_shared/elements/Controls.lua
+sed -i 's/?Shuffle/?乱序播放/' scripts/uosc_shared/elements/Controls.lua
+sed -i 's/?Fullscreen/?切换全屏/' scripts/uosc_shared/elements/Controls.lua
+
+
+
+
