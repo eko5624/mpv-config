48d05f1f228256eff85c2707fcbf931db7983b9d
eko5624 <562433403@qq.com>
2022-11-29 18:22:11 +0800

    Update macos_config.sh

--- macos_config.sh
+++ macos_config.sh
@@ -2,11 +2,15 @@
 set -x
 
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+rm -rf $DIR/macos_config/fonts
+rm -rf $DIR/macos_config/script-opts
+rm -rf $DIR/macos_config/scripts
+mkdir -p $DIR/macos_config/script-opts
+mkdir -p $DIR/macos_config/scripts
 
 cd $DIR/macos_config 
 git clone https://github.com/hooke007/MPV_lazy.git --branch main
 
-rm -rf scripts/uosc
 cp -f MPV_lazy/portable_config/scripts/input_plus.lua scripts
 cp -f MPV_lazy/portable_config/scripts/playlistmanager.lua scripts
 cp -f MPV_lazy/portable_config/scripts/save_global_props.lua scripts
@@ -25,7 +29,6 @@ sed -i 's/cycle:not_started:pause:no=play_circle\/yes=pause_circle/cycle:play_ar
 # Change 'open file' icon from 'file_open' to 'folder'.
 sed -i 's/command:file_open/command:folder/' scripts/uosc/elements/Controls.lua
 
-rm -rf fonts
 cp -f MPV_lazy/portable_config/script-opts/console.conf script-opts
 cp -f MPV_lazy/portable_config/script-opts/osc.conf script-opts
 cp -f MPV_lazy/portable_config/script-opts/playlistmanager.conf script-opts
