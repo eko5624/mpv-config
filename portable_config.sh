c27ae9afbd1933633403481fa3e25853f7d62ea8
eko5624 <562433403@qq.com>
2024-08-16 11:01:42 +0800

    -

--- portable_config.sh
+++ portable_config.sh
@@ -15,14 +15,15 @@ cd $DIR/portable_config
 
 echo "Getting HDR Toys"
 echo "======================="
-curl -OL https://github.com/natural-harmonia-gropius/hdr-toys/releases/download/v2308/HDR-Toys.v2308.zip
-unzip HDR-Toys.v2308.zip
-cd "HDR-Toys v2308"
+curl -OL https://github.com/natural-harmonia-gropius/hdr-toys/releases/download/v2401/HDR-Toys.v2401.zip
+unzip HDR-Toys.v2401.zip
+cd "HDR-Toys v2401"
 cp -f scripts/hdr-toys-helper.lua ../scripts
+cp -f hdr-toys.conf ../
 cp -rf shaders ../
 cd ..
-rm -rf "HDR-Toys v2308"
-rm HDR-Toys.v2308.zip
+rm -rf "HDR-Toys v2401"
+rm HDR-Toys.v2401.zip
 
 echo "Getting mpv-menu-plugin"
 echo "======================="
@@ -37,6 +38,8 @@ rm menu.zip
 echo "Getting thumbfast"
 echo "======================="
 git clone https://github.com/po5/thumbfast.git --branch master
+curl -O https://raw.githubusercontent.com/po5/thumbfast/vanilla-osc/player/lua/osc.lua
+mv -f ./osc.lua scripts
 cp -f thumbfast/thumbfast.lua scripts  
 cp -f thumbfast/thumbfast.conf script-opts
 rm -rf thumbfast
@@ -47,58 +50,37 @@ sed -i 's/max_width=200/max_width=360/' script-opts/thumbfast.conf
 sed -i 's/hwdec=no/hwdec=yes/' script-opts/thumbfast.conf
 sed -i 's/direct_io=no/direct_io=yes/' script-opts/thumbfast.conf
 
-echo "Getting uosc"
-echo "======================="
-git clone https://github.com/tomasklaen/uosc.git --branch main
-rm -rf fonts
-cp -r uosc/src/fonts ./
-cp -rf uosc/src/uosc scripts
-cp -f uosc/src/uosc.conf script-opts
-rm -rf uosc
+#echo "Getting uosc"
+#echo "======================="
+#git clone https://github.com/tomasklaen/uosc.git --branch main
+#rm -rf fonts
+#cp -r uosc/src/fonts ./
+#cp -rf uosc/src/uosc scripts
+#cp -f uosc/src/uosc.conf script-opts
+#rm -rf uosc
 
 # Change default languages to zh-hans.
-sed -i 's/languages=slang,en/languages=zh-hans/' script-opts/uosc.conf
+#sed -i 's/languages=slang,en/languages=zh-hans/' script-opts/uosc.conf
 
 # Enable autoload.
-sed -i 's/autoload=no/autoload=yes/' script-opts/uosc.conf
+#sed -i 's/autoload=no/autoload=yes/' script-opts/uosc.conf
 
 # Don't dim screen when menu triggered.
-sed -i 's/opacity=/opacity=curtain=0/' script-opts/uosc.conf
+#sed -i 's/opacity=/opacity=curtain=0/' script-opts/uosc.conf
 
 # Don't show timeline when paused.
-sed -i 's/timeline_persistency=paused/timeline_persistency=idle,audio/' script-opts/uosc.conf
+#sed -i 's/timeline_persistency=paused/timeline_persistency=idle,audio/' script-opts/uosc.conf
 
 # Change the highth of timeline to 40pixels when in fullscreen.
-sed -i 's/timeline_size_max_fullscreen=60/timeline_size_max_fullscreen=40/' script-opts/uosc.conf
+#sed -i 's/timeline_size_max_fullscreen=60/timeline_size_max_fullscreen=40/' script-opts/uosc.conf
 
 # Add 'stats' 'open file' 'prev/next chapter' 'chapter' buttons.
-sed -i 's/menu,gap/menu,script-stats,open-file,gap,<has_chapter>command:skip_previous:add chapter -1?上一章节,play_pause,<has_chapter>command:skip_next:add chapter 1?下一章节,<has_chapter>chapters,gap/' script-opts/uosc.conf
-sed -i 's/<has_many_audio>audio/audio/' script-opts/uosc.conf
+#sed -i 's/menu,gap/menu,script-stats,open-file,gap,<has_chapter>command:skip_previous:add chapter -1?上一章节,play_pause,<has_chapter>command:skip_next:add chapter 1?下一章节,<has_chapter>chapters,gap/' script-opts/uosc.conf
+#sed -i 's/<has_many_audio>audio/audio/' script-opts/uosc.conf
 
 # Add 'play/pause' 'stats' buttons.
-sed -i "/subtitles =/i \\\t\t['play_pause'] = 'cycle:play_arrow:pause:no=pause\/yes=play_arrow?播放\/暂停'," scripts/uosc/elements/Controls.lua
-sed -i "/subtitles =/a \\\t\t['script-stats'] = 'command:analytics:script-binding stats/display-stats-toggle?统计数据'," scripts/uosc/elements/Controls.lua
-
-# Add simplified chinese translation.
-#sed -i 's/?Menu/?菜单/' scripts/uosc/elements/Controls.lua
-#sed -i 's/?Subtitles/?字幕轨/' scripts/uosc/elements/Controls.lua
-#sed -i "s/?Audio',/?音频轨',/" scripts/uosc/elements/Controls.lua
-#sed -i 's/?Audio device/?音频设备/' scripts/uosc/elements/Controls.lua
-#sed -i 's/?Video/?视频轨/' scripts/uosc/elements/Controls.lua
-#sed -i "s/?Playlist',/?播放列表',/" scripts/uosc/elements/Controls.lua
-#sed -i 's/?Chapters/?章节/' scripts/uosc/elements/Controls.lua
-#sed -i 's/?Editions/?版本/' scripts/uosc/elements/Controls.lua
-#sed -i 's/?Stream quality/?流品质/' scripts/uosc/elements/Controls.lua
-#sed -i 's/?Open file/?加载文件/' scripts/uosc/elements/Controls.lua
-#sed -i 's/?Playlist\/Files/?播放列表\/文件浏览器/' scripts/uosc/elements/Controls.lua
-#sed -i 's/?Previous/?上一个/' scripts/uosc/elements/Controls.lua
-#sed -i 's/?Next/?下一个/' scripts/uosc/elements/Controls.lua
-#sed -i 's/?First/?首位/' scripts/uosc/elements/Controls.lua
-#sed -i 's/?Last/?末位/' scripts/uosc/elements/Controls.lua
-#sed -i 's/?Loop playlist/?列表循环/' scripts/uosc/elements/Controls.lua
-#sed -i 's/?Loop file/?单曲循环/' scripts/uosc/elements/Controls.lua
-#sed -i 's/?Shuffle/?乱序播放/' scripts/uosc/elements/Controls.lua
-#sed -i 's/?Fullscreen/?切换全屏/' scripts/uosc/elements/Controls.lua
+#sed -i "/subtitles =/i \\\t\t['play_pause'] = 'cycle:play_arrow:pause:no=pause\/yes=play_arrow?播放\/暂停'," scripts/uosc/elements/Controls.lua
+#sed -i "/subtitles =/a \\\t\t['script-stats'] = 'command:analytics:script-binding stats/display-stats-toggle?统计数据'," scripts/uosc/elements/Controls.lua
 
 echo "Getting SmartCopyPaste_II.lua"
 echo "======================="
@@ -118,6 +100,8 @@ curl -O https://raw.githubusercontent.com/christoph-heinrich/mpv-quality-menu/ma
 mv -f ./quality-menu.lua scripts
 curl -O https://raw.githubusercontent.com/christoph-heinrich/mpv-quality-menu/master/quality-menu.conf
 mv -f ./quality-menu.conf script-opts
+curl -O https://raw.githubusercontent.com/christoph-heinrich/mpv-quality-menu/master/quality-menu-osc.lua
+mv -f ./quality-menu-osc.lua scripts
 
 #echo "Getting autoload"
 #echo "======================="
