52ca06e808a225f833aaba0666c081e8d340f651
eko5624 <562433403@qq.com>
2025-02-13 22:15:15 +0800

    macos_config: add ModernZ, remove uosc

--- macos_config.sh
+++ macos_config.sh
@@ -8,6 +8,7 @@ rm -rf $DIR/macos_config/script-opts
 rm -rf $DIR/macos_config/scripts
 mkdir -p $DIR/macos_config/script-opts
 mkdir -p $DIR/macos_config/scripts
+mkdir -p $DIR/macos_config/fonts
 
 cd $DIR/macos_config
 echo "Getting thumbfast"
@@ -20,62 +21,22 @@ cp -f thumbfast/thumbfast.conf script-opts
 rm -rf thumbfast
 
 #Change thumb size from 200px to 360px
-sed -i 's/max_height=200/max_height=360/' script-opts/thumbfast.conf
-sed -i 's/max_width=200/max_width=360/' script-opts/thumbfast.conf
+sed -i 's/max_height=200/max_height=350/' script-opts/thumbfast.conf
+sed -i 's/max_width=200/max_width=350/' script-opts/thumbfast.conf
 sed -i 's/hwdec=no/hwdec=yes/' script-opts/thumbfast.conf
 sed -i 's/direct_io=no/direct_io=yes/' script-opts/thumbfast.conf
 
-echo "Getting uosc"
+echo "Getting ModernZ"
 echo "============"
-git clone https://github.com/tomasklaen/uosc.git --branch main
-cp -r uosc/src/fonts ./
-cp -rf uosc/src/uosc scripts
-cp -f uosc/src/uosc.conf script-opts
-rm -rf uosc
+git clone https://github.com/Samillion/ModernZ.git --branch master
+cp -f ModernZ/fluent-system-icons.ttf fonts
+cp -f ModernZ/modernz.lua scripts
+cp -f ModernZ/extras/pause-indicator-lite/pause_indicator_lite.lua scripts
+cp -f ModernZ/modernz.conf script-opts
+cp -f ModernZ/extras/locale/modernz-locale.json script-opts
 
-# Change default languages to zh-hans.
-sed -i 's/languages=slang,en/languages=zh-hans/' script-opts/uosc.conf
-
-# Enable autoload.
-sed -i 's/autoload=no/autoload=yes/' script-opts/uosc.conf
-
-# Don't dim screen when menu triggered.
-sed -i 's/opacity=/opacity=curtain=0/' script-opts/uosc.conf
-
-# Don't show timeline when paused.
-sed -i 's/timeline_persistency=paused/timeline_persistency=idle,audio/' script-opts/uosc.conf
-
-# Change the highth of timeline to 40pixels when in fullscreen.
-sed -i 's/timeline_size_max_fullscreen=60/timeline_size_max_fullscreen=40/' script-opts/uosc.conf
-
-# Add 'stats' 'open file' 'prev/next chapter' 'chapter' buttons.
-sed -i 's/menu,gap/menu,script-stats,open-file,gap,<has_chapter>command:skip_previous:add chapter -1?上一章节,play_pause,<has_chapter>command:skip_next:add chapter 1?下一章节,<has_chapter>chapters,gap/' script-opts/uosc.conf
-sed -i 's/<has_many_audio>audio/audio/' script-opts/uosc.conf
-
-# Add 'play/pause' 'stats' buttons.
-sed -i "/subtitles =/i \\\t\t['play_pause'] = 'cycle:play_arrow:pause:no=pause\/yes=play_arrow?播放\/暂停'," scripts/uosc/elements/Controls.lua
-sed -i "/subtitles =/a \\\t\t['script-stats'] = 'command:analytics:script-binding stats/display-stats-toggle?统计数据'," scripts/uosc/elements/Controls.lua
-
-# Add simplified chinese translation. No need anymore.
-#sed -i 's/?Menu/?菜单/' scripts/uosc/elements/Controls.lua
-#sed -i 's/?Subtitles/?字幕轨/' scripts/uosc/elements/Controls.lua
-#sed -i "s/?Audio',/?音频轨',/" scripts/uosc/elements/Controls.lua
-#sed -i 's/?Audio device/?音频设备/' scripts/uosc/elements/Controls.lua
-#sed -i 's/?Video/?视频轨/' scripts/uosc/elements/Controls.lua
-#sed -i "s/?Playlist',/?播放列表',/" scripts/uosc/elements/Controls.lua
-#sed -i 's/?Chapters/?章节/' scripts/uosc/elements/Controls.lua
-#sed -i 's/?Editions/?版本/' scripts/uosc/elements/Controls.lua
-#sed -i 's/?Stream quality/?流品质/' scripts/uosc/elements/Controls.lua
-#sed -i 's/?Open file/?加载文件/' scripts/uosc/elements/Controls.lua
-#sed -i 's/?Playlist\/Files/?播放列表\/文件浏览器/' scripts/uosc/elements/Controls.lua
-#sed -i 's/?Previous/?上一个/' scripts/uosc/elements/Controls.lua
-#sed -i 's/?Next/?下一个/' scripts/uosc/elements/Controls.lua
-#sed -i 's/?First/?首位/' scripts/uosc/elements/Controls.lua
-#sed -i 's/?Last/?末位/' scripts/uosc/elements/Controls.lua
-#sed -i 's/?Loop playlist/?列表循环/' scripts/uosc/elements/Controls.lua
-#sed -i 's/?Loop file/?单曲循环/' scripts/uosc/elements/Controls.lua
-#sed -i 's/?Shuffle/?乱序播放/' scripts/uosc/elements/Controls.lua
-#sed -i 's/?Fullscreen/?切换全屏/' scripts/uosc/elements/Controls.lua
+#Change ModernZ language from en to zh
+sed -i 's/language=en/language=zh/' script-opts/modernz.conf
 
 echo "Getting SmartCopyPaste_II"
 echo "============"
